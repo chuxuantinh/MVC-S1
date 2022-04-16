a55ed67449c2a9a15f758b42dd11f46257c31bc1
Chu Xuân Tình <50776038+chuxuantinh@users.noreply.github.com>
2022-04-16 16:47:13 +0700

    Initial commit

---
+++ MVC-S1/Controllers/EmployeeController.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC_S1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MVC_S1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly TinhCompanyDBContext _context;
+        public EmployeeController(TinhCompanyDBContext context)
+        {
+            _context = context;
+        }
+        // GET: EmployeeController
+        //public ActionResult Index()
+        //{
+          //  return View();
+        // }
+
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        //public ActionResult Details(int id)
+        //{
+         //   return View();
+        //}
+
+        // GET: EmployeeController/Create
+
+
+        //AddOrEdit Get Method
+        public async Task<IActionResult> AddOrEdit(int? employeeId)
+        {
+            ViewBag.PageName = employeeId == null ? "Create Employee" : "Edit Employee";
+            ViewBag.IsEdit = employeeId == null ? false : true;
+            if (employeeId == null)
+            {
+                return View();
+            }
+            else
+            {
+                var employee = await _context.Employees.FindAsync(employeeId);
+
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                return View(employee);
+            }
+        }
+
+        //AddOrEdit Post Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(int employeeId, [Bind("EmployeeId,Name,Designation,Address,Salary,JoiningDate")]
+            Employee employeeData)
+        {
+            bool IsEmployeeExist = false;
+
+            Employee employee = await _context.Employees.FindAsync(employeeId);
+
+            if (employee != null)
+            {
+                IsEmployeeExist = true;
+            }
+            else
+            {
+                employee = new Employee();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    employee.Name = employeeData.Name;
+                    employee.Designation = employeeData.Designation;
+                    employee.Address = employeeData.Address;
+                    employee.Salary = employeeData.Salary;
+                    employee.JoiningDate = employeeData.JoiningDate;
+
+                    if (IsEmployeeExist)
+                    {
+                        _context.Update(employee);
+                    }
+                    else
+                    {
+                        _context.Add(employee);
+                    }
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employeeData);
+        }
+
+
+        // Employee Details
+        public async Task<IActionResult> Details(int? employeeId)
+        {
+            if (employeeId == null)
+            {
+                return NotFound();
+            }
+            var employee = await _context.Employees.FirstOrDefaultAsync(m => m.EmployeeId == employeeId);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+
+
+        // GET: EmployeeController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: EmployeeController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+
+        public async Task<IActionResult> Delete(int? employeeId)
+        {
+            if (employeeId == null)
+            {
+                return NotFound();
+            }
+            var employee = await _context.Employees.FirstOrDefaultAsync(m => m.EmployeeId == employeeId);
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/1
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int employeeId)
+        {
+            var employee = await _context.Employees.FindAsync(employeeId);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ MVC-S1/Models/TinhCompanyDBContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+
+namespace MVC_S1.Models
+{
+    public partial class TinhCompanyDBContext : DbContext
+    {
+     
+
+        public TinhCompanyDBContext(DbContextOptions<TinhCompanyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-6PVLS4F\\CHUTINH;Database=Tinh-CompanyDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Address)
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.JoiningDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
